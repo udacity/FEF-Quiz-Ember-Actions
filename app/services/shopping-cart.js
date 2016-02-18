b2d226ada343f9c85150b6576f08bbdf806ee5cc
Richard Kalehoff <richardkalehoff@gmail.com>
2016-02-18 15:55:19 -0800

    cart displays all content in shopping-cart service

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  cart: [
+    {color: 'red', size: '1 x 1', price: '0.01'},
+    {color: 'blue', size: '2 x 4', price: '0.04'},
+    {color: 'green', size: '2 x 8', price: '0.08'},
+  ],
+
+  getCart() {
+    return this.get('cart');
+  }
+
 });
