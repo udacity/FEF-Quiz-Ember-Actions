3e8d14befe55c02a54469d975dd7a976929f0ffe
Richard Kalehoff <richardkalehoff@gmail.com>
2016-02-18 15:59:35 -0800

    add addToCart action to bricks-a-bunch

--- app/routes/store/bricks-a-bunch.js
+++ app/routes/store/bricks-a-bunch.js
@@ -3,6 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   warehouse: Ember.inject.service('brick-warehouse'),
 
+  actions: {
+    addToCart: function(color, size, price) {
+      console.log(color, size, price);
+    }
+  },
+
   model() {
     return this.get('warehouse').getRedBricks();
   }
